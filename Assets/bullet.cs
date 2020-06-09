ec140ab998e9703696cdbfde72d5e05ec4afbe28
Mahmood-Darwish <m.darwish@innopolis.university>
2020-06-09 16:05:47 +0300

    Added bullets for the first build.

---
+++ Assets/bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    float timer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/controller.cs
+++ Assets/controller.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class controller : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField]
+    GameObject Bullet;
+    float CD;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,19 +18,29 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CD = Mathf.Max(0, CD - Time.deltaTime);
         if (Input.touchCount > 0 || Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = new Vector3(5, 6, 0); 
+            rb.velocity = new Vector3(10, 10, 0); 
             transform.eulerAngles = new Vector3(
                  transform.eulerAngles.x,
                  transform.eulerAngles.y,
                  -45
              );
-
+            
+            if(Input.GetTouch(0).phase == TouchPhase.Began && CD == 0)
+            {
+                CD = .5f;
+                Vector3 pos = transform.position;
+                Quaternion rotation = transform.rotation;
+                GameObject temp = Instantiate(Bullet, pos, rotation);
+                temp.transform.position = temp.transform.position + new Vector3(1, 0, 0);
+                temp.GetComponent<Rigidbody>().velocity = new Vector3(20, 0, 0);
+            }
         }
         else
         {
-            rb.velocity = new Vector3(5, -6, 0);
+            rb.velocity = new Vector3(10, -10, 0);
             transform.eulerAngles = new Vector3(
                  transform.eulerAngles.x,
                  transform.eulerAngles.y,
