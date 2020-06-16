59a3c70a167f8f0cd669bcc7ae0b678f102b7ca5
Mahmood-Darwish <m.darwish@innopolis.university>
2020-06-16 14:44:34 +0300

    added sliding wall

---
+++ Assets/Scripts/SlidingWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingWall : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Rigidbody rb;
+    Renderer rd;
+
+    void Start()
+    {
+        rd = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rd.isVisible)
+        {
+            rb.velocity = new Vector3(0, -speed, 0);
+        }
+    }
+}
